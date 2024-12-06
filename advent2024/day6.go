30c975717acc4fd1bcb57a81628f77c5989e7496
Dallas S. <andrewdallassherman@gmail.com>
2024-12-06 09:03:30 +0900

    forgot to add my input to the file for quicker start

--- advent2024/day6.go
+++ advent2024/day6.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
-
-
 func day6_1(input string) {
- fmt.Println("Day 6 part 1 is: ", output)
+	lines := strings.Split(strings.TrimSpace(input), "\n")
+	output := 0
+
+	fmt.Println("Day 6 part 1 is: ", output)
 }
 
 // func day6_2(input string) {
