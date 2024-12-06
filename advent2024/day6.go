d6e43a89ff10507ad324122da448f640131f8117
Dallas S. <andrewdallassherman@gmail.com>
2024-12-06 22:21:33 +0900

    doing a little bit of day 6, can't focus going to finish tomorrow

--- advent2024/day6.go
+++ advent2024/day6.go
@@ -7,8 +7,33 @@ import (
 
 func day6_1(input string) {
 	lines := strings.Split(strings.TrimSpace(input), "\n")
+	grid := make([][]rune, len(lines))
 	output := 0
 
+	guardRow := -1
+	guardCol := -1
+	guardDirection := 0
+
+	for _, row := range grid {
+		if guardRow >= 0 {
+			break
+		}
+		for col, _:= range grid[row] {
+			if grid[row][col] == '^' {
+				guardRow = row
+				guardCol = col
+				posFoune = true
+				break
+
+			}
+		}
+	}
+	directions := [][2]int{
+		{-1, 0},
+		{0, 1},
+		{1, 0},
+		{-1, 0}
+	}
 	fmt.Println("Day 6 part 1 is: ", output)
 }
 
